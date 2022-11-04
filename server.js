fbe1038c9c4f3da6829b400ca862bab9fe45c5b1
Diego BustamantediegoBustamante <diegi.b58@gmail.com>
2022-11-03 22:29:57 -0300

    entrega 4

---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const app = express();
+
+const {
+    Router
+} = express;
+const Container = require('./container');
+const container = new Container();
+const routerProducts = Router();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+app.use('/api/products', routerProducts);
+
+app.listen(port, () => console.log(`The server is running in: http://localhost:${port} `));
+app.get('/', (req, res) => {
+    res.send("<h1 >CHECK OUR PRODUCTS</h1>");
+});
+app.get('/form', (req, res) => {
+    res.sendFile(__dirname + '/publics/index.html');
+});
+
+app.post('/form', (req, res) => {
+    const {
+        body
+    } = req;
+    console.log(body);
+    container.save(body);
+    res.send('Product uploaded');
+});
+
+routerProducts.get('', async (req, res) => {
+    const products = await container.getAll();
+    res.json(products);
+});
+
+routerProducts.get('/:id', async (req, res) => {
+    const {
+        id
+    } = req.params;
+    const products = await container.getAll();
+    const product = await container.getById(id);
+    if (id > products.length) {
+        res.json({
+            error: 'This product was not found',
+            productList: products,
+        });
+    } else {
+        res.json(product);
+    }
+});
+
+routerProducts.delete('/:id', async (req, res) => {
+    const {
+        id
+    } = req.params;
+    const products = await container.getAll();
+
+    if (id > products.length) {
+        res.json({
+            error: 'This product was not found',
+            productList: products,
+        });
+    } else {
+        await container.deleteById(id);
+        res.json({
+            success: true,
+            msg: 'This product was deleted',
+        });
+    }
+});
+
+routerProducts.put('/:id', async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params;
+        const {
+            title,
+            price,
+            thumbnail
+        } = req.body;
+        await container.updateById(title, price, thumbnail, id);
+        res.json({
+            succes: true
+        });
+    } catch (error) {
+        res.json({
+            error: true,
+            msj: 'error'
+        });
+    }
+});
